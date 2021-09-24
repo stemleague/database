5ad6794269a3c5d48b0906485f7d5503661b15f2
stem league <64894084+stemleague@users.noreply.github.com>
2021-09-24 12:32:05 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,76 @@
+# Welcome to the Database Tracker! Here we will start by introducing ourselves, calculate discount amount and discounted price for a product on sale, and fixing the bugs for a popular nursery rhyme!
+
+# Here is our main method for our program, where Python starts reading our code and executing commands
+if __name__ == "__main__":
+  ## Part A ##
+  # String: assign a variable called (name) to your name
+  # Integer: assign a variable called (grade) to your grade level
+  # Float: assign a variable called (weeks) to the value 5.0
+  
+  # Here you are introducing yourself with a print statement and concatenation with a (+) sign to connect strings, integers, and floats together
+  # Note that the variables (grade) and (weeks) are enclosed in str(). We use str() to typecast integers and floats to a string data type. This is crucial so that this print statement can be executed and the computer can read in your information.
+  print("Hello everyone! My name is " + name + " and I am in " + str(grade) + "th grade. I am a Data Programming student in the " + str(weeks) + " weeks long Explorer
+  Program!")
+  
+  ## Part B ##
+  # Price of Product 
+  price: 1.30
+  
+  # Discount Percentage
+  discount_percentage: 50.0 
+  
+  # Discount Amount
+  discount_amount: 0
+  
+  # Discounted Price
+  discounted_price: 0
+  
+  # Formula to Calculate Price for a Product on Sale
+  # Discount Amount = (Discount Percentage x Price)/100
+  # Discounted Price = Price - Discount Amount
+  
+  # Replace the formula with the variables indicated above for Part B and assign to variables called (discount_amount) and (discounted_price)
+  #
+  #
+  
+  # Use print statements to print out the discount amount and the discounted price
+  #
+  #
+  
+  ## Part C ##
+  q = # Replace this comment with the escape sequence for a quote
+  
+  # Use your knowledge of escape sequences to fix the bugs that occur when this code segment is run
+  # Replace quotation marks in this nursery rhyme with the variable (q) to print quotation marks without errors in the code!
+  
+  print("Try reading aloud the Betty Botter Rhyme!")
+  print("Betty Botter bought a bit of butter;")
+  print(“But,” she said, “this butter’s bitter!")
+  print("If I put it in my batter")
+  print("It will make my batter bitter.")
+  print("But a bit o’ better butter")
+  print("Will make my batter better.”")
+
+  print("Then she bought a bit o’ butter")
+  print("Better than the bitter butter,")
+  print("Made her bitter batter better.")
+  print("So ’twas better Betty Botter")
+  print("Bought a bit o’ better butter.")
+  
+
+# Here is the Betty Botter Rhyme for reference
+# Betty Botter Rhyme
+# Betty Botter bought a bit of butter;
+# “But,” she said, “this butter’s bitter!
+# If I put it in my batter
+# It will make my batter bitter.
+# But a bit o’ better butter
+# Will make my batter better.”
+
+# Then she bought a bit o’ butter
+# Better than the bitter butter,
+# Made her bitter batter better.
+# So ’twas better Betty Botter
+# Bought a bit o’ better butter.
+  
+  
